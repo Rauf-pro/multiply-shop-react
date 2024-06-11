8583fc164ebc2fc357b45e50398a59a930a64cfb
rrauf8178 <rrauf8178@gmail.com>
2024-06-11 17:02:22 +0400

    add: features to home page

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { features } from "../data/Data";
+
 
 export default function Features() {
   return (
-    <div>Features</div>
+    <>
+      <div className="container-fluid pt-5">
+        <div className="row px-xl-5 pb-3">
+          {features.map((val, ky) => (
+            <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
+              <div
+                className="d-flex align-items-center bg-light mb-4"
+                style={{ padding: "30px" }}
+              >
+                {val.icon}
+                <h5 className="font-weight-semi-bold m-0">{val.title}</h5>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   )
 }
